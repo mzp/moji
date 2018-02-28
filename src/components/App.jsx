4eef0ef8b3da275c7c6ceeba8f63ffbdea86abc2
mzp <mzpppp@gmail.com>
2018-02-28 09:02:31 +0900

    :hocho: extract glyph definition

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,27 +3,15 @@ import React from 'react';
 import Slide from './Slide';
 import SlideShow from './SlideShow';
 import Style from '../style';
-
-const slides = [
-  [
-    { char: '柿', codepoint: 'U+67FF', name: 'KAKI', mean: 'Persimon' },
-    { char: '杮', codepoint: 'U+676E', name: 'KOKERA', mean: 'Wood board' }
-  ],
-  [
-    { char: '高', codepoint: 'U+9AD8', name: 'KUCHI-DAKA', mean: 'High' },
-    { char: '髙', codepoint: 'U+9AD9', name: 'HASHIGO-DAKA', mean: 'High' }
-
-  ]
-];
+import definitions from '../definitions';
 
 export default class extends React.Component {
   render() {
-
     return (
       <div className={css(Style.root)}>
         <SlideShow transitionName='cross-fade' transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
-          {slides.map((slide) => {
-            return (<Slide key={slide[0].codepoint} chars={slide} />);
+          {definitions.map((definition) => {
+            return (<Slide key={definition[0].codepoint} chars={definition} />);
           })}
         </SlideShow>
       </div>
---
+++ src/definitions.js
@@ -0,0 +1,10 @@
+export default [
+  [
+    { char: '柿', codepoint: 'U+67FF', name: 'KAKI', mean: 'Persimon' },
+    { char: '杮', codepoint: 'U+676E', name: 'KOKERA', mean: 'Wood board' }
+  ],
+  [
+    { char: '高', codepoint: 'U+9AD8', name: 'KUCHI-DAKA', mean: 'High' },
+    { char: '髙', codepoint: 'U+9AD9', name: 'HASHIGO-DAKA', mean: 'High' }
+  ]
+];
