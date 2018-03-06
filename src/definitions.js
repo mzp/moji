c79bd0b165906d644a290e1983340746cc10583b
mzp <mzpppp@gmail.com>
2018-03-07 08:52:17 +0900

    add some glyph

--- src/definitions.js
+++ src/definitions.js
@@ -1,10 +1,21 @@
 export default [
   [
+    { char: '田', codepoint: 'U+7530', name: 'TA', mean: 'Field' },
+    { char: '由', codepoint: 'U+7531', name: 'YOSHI', mean: 'Origin' },
+    { char: '甲', codepoint: 'U+7532', name: 'KOU', mean: 'Shell' },
+    { char: '申', codepoint: 'U+7533', name: 'SARU', mean: 'Monkey' }
+  ],
+  [
     { char: '柿', codepoint: 'U+67FF', name: 'KAKI', mean: 'Persimon' },
     { char: '杮', codepoint: 'U+676E', name: 'KOKERA', mean: 'Wood board' }
   ],
   [
-    { char: '高', codepoint: 'U+9AD8', name: 'KUCHI-DAKA', mean: 'High' },
-    { char: '髙', codepoint: 'U+9AD9', name: 'HASHIGO-DAKA', mean: 'High' }
+    { char: '🐖', codepoint: 'U+1F416', name: 'EMOJI', mean: 'pig', lang: 'en' },
+    { char: '𓃟', codepoint: 'U+130DF', name: 'HIEROGLYPH', mean: 'pig' },
+  ],
+  [
+    { char: 'A', codepoint: 'U+0041', name: 'Roman A', mean: 'First letter', lang: 'en' },
+    { char: 'Α', codepoint: 'U+676E', name: 'Greek A', mean: 'First letter', lang: 'en' },
+    { char: 'А', codepoint: 'U+0410', name: 'Cyrillic A', mean: 'First letter', lang: 'en' }
   ]
 ];
