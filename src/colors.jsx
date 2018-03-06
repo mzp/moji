9b2b51967aa13c710759bfd3564b3e3918d14c99
mzp <mzpppp@gmail.com>
2018-03-07 08:56:44 +0900

    add colors

--- src/colors.jsx
+++ src/colors.jsx
@@ -1,4 +1,5 @@
 export default [
   { color: 'white', backgroundColor: 'rgb(64, 133,108)' },
-  { color: 'white', backgroundColor: 'rgb(136, 167, 91) ' }
+  { color: 'white', backgroundColor: 'rgb(136, 167, 91)' },
+  { color: 'white', backgroundColor: 'rgb(16, 115, 159)' }
 ];
