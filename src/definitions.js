e36afd77fa5330f598ab3e1930245c901105d746
mzp <mzpppp@gmail.com>
2018-03-07 21:50:46 +0900

    add moji

--- src/definitions.js
+++ src/definitions.js
@@ -11,11 +11,143 @@ export default [
   ],
   [
     { char: '🐖', name: 'EMOJI', mean: 'pig', lang: 'en' },
-    { char: '𓃟',  name: 'HIEROGLYPH', mean: 'pig' },
+    { char: '𓃟',  name: 'HIEROGLYPH', mean: 'pig' }
   ],
   [
     { char: 'A', name: 'Roman A', mean: 'First letter', lang: 'en' },
     { char: 'Α', name: 'Greek A', mean: 'First letter', lang: 'en' },
     { char: 'А', name: 'Cyrillic A', mean: 'First letter', lang: 'en' }
+  ],
+  [
+    { char: '日', name: 'HI', mean: 'Day' },
+    { char: '曰', name: 'IWAKU', mean: 'Say' }
+  ],
+  [
+    { char: '夭', name: 'WAKAI', mean: 'Young' },
+    { char: '天', name: 'TEN', mean: 'Sky' }
+  ],
+  [
+    { char: '未', name: 'IMADA', mean: 'Not yet' },
+    { char: '末', name: 'SUE', mean: 'End' }
+  ],
+  [
+    { char: '王', name: 'OU', mean: 'King' },
+    { char: '壬', name: 'MIZUNOE', mean: 'Water' }
+  ],
+  [
+    { char: '毋', name: 'NAKARE', mean: 'Don\'t' },
+    { char: '毌', name: 'TURANUKU', mean: 'Penetrate' },
+    { char: '母', name: 'HAHA', mean: 'Mother' }
+  ],
+  [
+    { char: '市', name: 'SI', mean: 'City' },
+    { char: '巿', name: 'FUTU', mean: 'Lap blanket' }
+  ],
+  [
+    { char: '刊', name: 'KAN', mean: 'Publication' },
+    { char: '刋', name: 'SEN', mean: 'Shave' }
+  ],
+  [
+    { char: '氾', name: 'HAN', mean: 'Stream' },
+    { char: '汜', name: 'SI', mean: 'Stream' }
+  ],
+  [
+    { char: '束', name: 'TABA', mean: 'Bundle' },
+    { char: '朿', name: 'TOGE', mean: 'Thorn' }
+  ],
+  [
+    { char: '句', name: 'KU', mean: 'Phrase' },
+    { char: '匂', name: 'NIOI', mean: 'Smell' },
+    { char: '勾', name: 'MAGARI', mean: 'Shaped' }
+  ],
+  [
+    { char: '戊', name: 'TUCHINOE', mean: 'Soil' },
+    { char: '戌', name: 'INU', mean: 'Dog' }
+  ],
+  [
+    { char: '盂', name: 'U', mean: 'Bowl' },
+    { char: '孟', name: 'MOU', mean: 'Chief' }
+  ],
+  [
+    { char: '裏', name: 'URA', mean: 'Back' },
+    { char: '裹', name: 'TUTUMU', mean: 'Wrap' }
+  ],
+  [
+    { char: '女', name: 'ONNA', mean: 'Woman' },
+    { char: '奻', name: 'DAN', mean: 'Bicker' },
+    { char: '姦', name: 'KASHIMASHI', mean: 'Noisy' }
+  ],
+  [
+    { char: '月', name: 'TUKI', mean: 'Moon' },
+    { char: '朋', name: 'TOMO', mean: 'Friend' }
+  ],
+  [
+    { char: '犬', name: 'INU', mean: 'Dog' },
+    { char: '猋', name: 'TSUMUJI-KAZE', mean: 'Whirlwind' }
+  ],
+  [
+    { char: '木', name: 'KI', mean: 'Tree' },
+    { char: '林', name: 'HAYASHI', mean: 'Forest' },
+    { char: '森', name: 'MORI', mean: 'Woods' }
+  ],
+  [
+    { char: '牛', name: 'USHI', mean: 'Cow' },
+    { char: '犇', name: 'HISHIMEKU', mean: 'Crowd' }
+  ],
+  [
+    { char: '火', name: 'HI', mean: 'Fire' },
+    { char: '炎', name: 'HONO', mean: 'Fire' },
+    { char: '焱', name: 'HONO', mean: 'Fire' },
+    { char: '燚', name: 'HONO', mean: 'Fire' }
+  ],
+  [
+    { char: '石', name: 'ISHI', mean: 'Stone' },
+    { char: '磊', name: 'RAI', mean: 'Many stone' }
+  ],
+  [
+    { char: '貝', name: 'KAI', mean: 'Shell' },
+    { char: '贔', name: 'HI', mean: 'Strong' }
+  ],
+  [
+    { char: '日', name: 'HI', mean: 'Shell' },
+    { char: '昌', name: 'SHOU', mean: 'Prosperous' },
+    { char: '晶', name: 'SHOU', mean: 'Crystal' }
+  ],
+  [
+    { char: '水', name: 'MIZU', mean: 'Water' },
+    { char: '淼', name: 'BYOU', mean: 'Many water' }
+  ],
+  [
+    { char: '毛', name: 'MIZU', mean: 'Hair' },
+    { char: '毳', name: 'MUKUGE', mean: 'Soft feather' }
+  ],
+  [
+    { char: '吟', name: 'UMEKU', mean: 'Chant' },
+    { char: '含', name: 'FUKUMU', mean: 'Include' }
+  ],
+  [
+    { char: '架', name: 'KAKERU', mean: 'Hang' },
+    { char: '枷', name: 'KASE', mean: 'Shackles' }
+  ],
+  [
+    { char: '棘', name: 'TOGE', mean: 'Thorn' },
+    { char: '棗', name: 'NATSUME', mean: 'Jujube' }
+  ],
+  [
+    { char: '脇', name: 'WAKI', mean: 'Side' },
+    { char: '脅', name: 'OBIYAKASU', mean: 'Threaten' }
+  ],
+  [
+    { char: '紋', name: 'MON', mean: 'Crest' },
+    { char: '紊', name: 'MOTURERU', mean: 'Disturb' }
+  ],
+  [
+    { char: '細', name: 'HOSOI', mean: 'Thin' },
+    { char: '累', name: 'KASANE', mean: 'Overlaid' }
+  ],
+  [
+    { char: '集', name: 'ATUMERU', mean: 'Collect' },
+    { char: '椎', name: 'SII', mean: 'Chinquapin' }
   ]
+
 ];
