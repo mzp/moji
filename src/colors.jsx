39f922364c9721bb0dd4a37a35e5a1d2021ea944
mzp <mzpppp@gmail.com>
2018-03-07 18:21:57 +0900

    add colors

--- src/colors.jsx
+++ src/colors.jsx
@@ -1,5 +1,11 @@
 export default [
   { color: 'white', backgroundColor: 'rgb(64, 133,108)' },
   { color: 'white', backgroundColor: 'rgb(136, 167, 91)' },
-  { color: 'white', backgroundColor: 'rgb(16, 115, 159)' }
+  { color: 'white', backgroundColor: 'rgb(16, 115, 159)' },
+  { color: 'white', backgroundColor: '#B71C1C' },
+  { color: 'white', backgroundColor: '#673AB7' },
+  { color: 'white', backgroundColor: '#827717' },
+  { color: 'white', backgroundColor: '#EF6C00' },
+  { color: 'white', backgroundColor: '#616161' },
+  { color: 'white', backgroundColor: '#607D8B' }
 ];
